f90c06f69687e1198b67917b6937ded49c9ec6fa
Rolexless <pepinero111@gmail.com>
2026-06-13 22:25:57 +0200

    Another fix

--- js/game.js
+++ js/game.js
@@ -1,11 +1,10 @@
 import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
 
-const k = kaboom(
-  {
-    canvas: document.getElementById("game"),
-    background: [20, 20, 30],
-  }
-);
+const k = kaboom({
+  canvas: document.getElementById("game"),
+  background: [20, 20, 30],
+  gravity: 0,
+});
 
 const TILE_SIZE = 32;
 
@@ -19,21 +18,8 @@ const map = [
   "################",
 ];
 
-const player = k.add([
-  k.rect(32, 32),
-  k.pos(0, 0),
-  k.color(0, 200, 255),
-  k.area(),
-  k.body(),
-]);
+let player;
 
-k.onUpdate(() => {
-  k.camPos(player.pos);
-});
-
-const SPEED = 200;
-
-// construir mapa
 map.forEach((row, y) => {
   row.split("").forEach((char, x) => {
     if (char === "#") {
@@ -47,18 +33,29 @@ map.forEach((row, y) => {
     }
 
     if (char === "P") {
-      player.pos = k.vec2(x * TILE_SIZE, y * TILE_SIZE);
+      player = k.add([
+        k.rect(32, 32),
+        k.pos(x * TILE_SIZE, y * TILE_SIZE),
+        k.color(0, 200, 255),
+        k.area(),
+        k.body(),
+      ]);
     }
   });
 });
 
-// movimiento
-k.onKeyDown("left", () => player.move(-SPEED, 0));
-k.onKeyDown("right", () => player.move(SPEED, 0));
-k.onKeyDown("up", () => player.move(0, -SPEED));
-k.onKeyDown("down", () => player.move(0, SPEED));
+k.onUpdate(() => {
+  if (player) k.camPos(player.pos);
+});
 
-k.onKeyDown("a", () => player.move(-SPEED, 0));
-k.onKeyDown("d", () => player.move(SPEED, 0));
-k.onKeyDown("w", () => player.move(0, -SPEED));
-k.onKeyDown("s", () => player.move(0, SPEED));
+const SPEED = 200;
+
+k.onKeyDown("left", () => player?.move(-SPEED, 0));
+k.onKeyDown("right", () => player?.move(SPEED, 0));
+k.onKeyDown("up", () => player?.move(0, -SPEED));
+k.onKeyDown("down", () => player?.move(0, SPEED));
+
+k.onKeyDown("a", () => player?.move(-SPEED, 0));
+k.onKeyDown("d", () => player?.move(SPEED, 0));
+k.onKeyDown("w", () => player?.move(0, -SPEED));
+k.onKeyDown("s", () => player?.move(0, SPEED));
